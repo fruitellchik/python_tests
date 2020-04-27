977cde9e44720972a8b72309dd3412f6090e7ebd
danil <danil.paletin@yandex.ru>
2020-04-27 21:00:57 +0300

    Оптимизирован код

--- Test_Login_Logout.py
+++ Test_Login_Logout.py
@@ -14,14 +14,29 @@ class TestLoginLogout(unittest.TestCase):
 
     def test_login_logout(self):
         wd = self.wd
-        wd.get("http://127.0.0.1/")
-        wd.find_element_by_id("username").clear()
-        wd.find_element_by_id("username").send_keys("admin")
-        wd.find_element_by_id("password").clear()
-        wd.find_element_by_id("password").send_keys("admin")
-        wd.find_element_by_link_text(u"Войти").click()
+        self.open_home_page(wd)
+        self.input_login(wd, username="admin")
+        self.input_password(wd, password="admin")
+        self.click_button_auth(wd)
+        self.click_button_logout(wd)
+
+    def click_button_logout(self, wd):
         wd.find_element_by_xpath("//div[@id='headContainer']/a/p/i").click()
-    
+
+    def click_button_auth(self, wd):
+        wd.find_element_by_link_text(u"Войти").click()
+
+    def input_password(self, wd, password):
+        wd.find_element_by_id("password").clear()
+        wd.find_element_by_id("password").send_keys(password)
+
+    def input_login(self, wd, username):
+        wd.find_element_by_id("username").clear()
+        wd.find_element_by_id("username").send_keys(username)
+
+    def open_home_page(self, wd):
+        wd.get("http://127.0.0.1/")
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
